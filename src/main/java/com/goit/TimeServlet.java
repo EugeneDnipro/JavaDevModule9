4926344ea4a9257934720beb40c00219784775f1
EugeneDnipro <EugeneDnepr@gmail.com>
2023-04-06 15:58:24 +0300

    Adding cookies in process

--- src/main/java/com/goit/TimeServlet.java
+++ src/main/java/com/goit/TimeServlet.java
@@ -5,6 +5,7 @@ import org.thymeleaf.context.Context;
 import org.thymeleaf.templateresolver.FileTemplateResolver;
 
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -20,6 +21,8 @@ import java.util.Map;
 public class TimeServlet extends HttpServlet {
     private TemplateEngine engine;
     String timezone;
+    HttpServletRequest req;
+    HttpServletResponse resp;
 
     @Override
     public void init() {
@@ -40,7 +43,7 @@ public class TimeServlet extends HttpServlet {
         timezone = req.getParameter("timezone");
 
         Map<String, Object> params = new LinkedHashMap<>();
-        params.put("timeString", getCurrentUtcTime(timezone));
+        params.put("timeString", getCurrentUtcTime(checkingLastTimezoneCookie(timezone, req, resp)));
 
         Context timeContext = new Context(
                 req.getLocale(),
@@ -51,15 +54,33 @@ public class TimeServlet extends HttpServlet {
         resp.getWriter().close();
     }
 
-
     public static String getCurrentUtcTime(String timezone) {
-        if (timezone == null) {
-            timezone = "UTC";
-        } else if (timezone.contains(" ")) {
-            timezone = timezone.replace(" ", "+");
-        }
+//        if (timezone == null) {
+//            timezone = "UTC";
+//        } else if (timezone.contains(" ")) {
+//            timezone = timezone.replace(" ", "+");
+//        }
         ZoneId zone = ZoneId.of(timezone);
         ZonedDateTime zonedDateTime = ZonedDateTime.now(zone);
         return zonedDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss '" + timezone + "'"));
+    }
+
+    public static String checkingLastTimezoneCookie(String tz, HttpServletRequest req, HttpServletResponse resp) {
+        if (tz != null && tz.contains(" ")) {
+            tz = tz.replace(" ", "+");
+        }
+
+        Cookie[] cookies = req.getCookies();
+        if (cookies.length == 0) {
+            if (tz == null) {
+                tz = "UTC";
+            } else {
+                resp.addCookie(new Cookie("lastTimezone", tz));
+            }
+        } else if (tz == null) {
+            tz = cookies[0].getValue();
+        }
+
+        return tz;
     }
 }
